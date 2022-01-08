a305ee7cf65c792b956da18f0b9e944158a596bd
Andre Johnson <andre.johnson@gmail.com>
2022-01-08 19:04:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+
+#some_list = ['a', 'b','c', 'b', 'd', 'm', 'n', 'n']
+
+#dups = []
+#for value in some_list:
+#  if some_list.count(value) > 1:
+#    if value not in dups:
+#      dups.append(value)
+
+#print(dups)
+
+#def say_hello():
+ # print("say hello")
+
+#say_hello()
+
+
+#def sum(num1, num2):
+ # '''
+  #eat a big dragon 
+  #'''
+  #return  num1 + num2
+
+#print(sum(10,5))
+
+#help(sum)
+
+#def super_func(*args):
+#  return sum(args)
+
+#super_func(1,2,3,4,5)
+
+#def highest_even(li):
+ # evens = []
+  #for value in li:
+   # if value % 2 == 0:
+    #  evens.append(value)
+ # return max(evens)
+  
+#print(highest_even([10,2,4,7,11,]))
+
+print("woof")
+a = 1
+def confusion():
+  a = 5
+  return a
+
+print(a)
+print(confusion())
